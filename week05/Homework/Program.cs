6bc6fc38c5d5d5937d3f1566b2f22a20faf6d9f4
jameseleight <jleight@byupathway.edu>
2026-01-30 22:40:45 -0800

    Update with Homework W05

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("James Leight", "Exponents");
+        MathAssignment mathAssignment1 = new MathAssignment("James Leight", "Exponents", "11.1", "12-23");
+        WritingAssignment writingAssignment1 = new WritingAssignment("James Leight", "History of the World", "What Mel Brooks Got Wrong");
+
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine();
+
+        Console.WriteLine(mathAssignment1.GetSummary());
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+        Console.WriteLine();
+        
+        Console.WriteLine(writingAssignment1.GetSummary());
+        Console.WriteLine(writingAssignment1.GetWritingInformation());
     }
 }
---
+++ week05/Homework/WritingAssignment.cs
@@ -0,0 +1,14 @@
+using System;
+public class WritingAssignment : Assignment
+{
+    private string _title;
+    public WritingAssignment(string studentName, string topic, string title)
+        : base(studentName, topic)
+    {
+        _title = title;
+    }
+    public string GetWritingInformation()
+    {
+        return $"Section {_title} by {_studentName}";
+    }
+}
